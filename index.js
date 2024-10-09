998b3c36d46994f3b0659182c0b90bc4c3583dce
Roeland Reyniers <roeland@bythepixel.com>
2024-10-09 13:13:21 -0600

    Test action

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const core = require('@actions/core');
+const fs = require("fs/promises");
 const github = require('@actions/github');
+
+const checklistPath = core.getInput('checklist_path')
+
+const checklistContent = await fs.readFile(checklistPath, "utf8");
+
+console.log(checklistContent);
 
 console.log('we made it!');
